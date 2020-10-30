0cbebed4761cbaf2a3a26c2784dd6fce649c5bc1
EngelberAmaya <engelberamaya@gmail.com>
2020-10-29 21:16:31 -0430

    First commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# ScanApp
+
+Esta es una App que utiliza los servicios de Generador de códigos QR-Online qrcode para tomar de prueba los código qr de página web y los mapas de acuerdo con una latitud y longitud para luego ser usado esos datos en los servicios de 3D-Buildings-Mapbox, para ver dichas coordenadas. De igual un listado con dichos escáner, dependiendo si es un link de una página web, se podrá ser redirigido a la misma y si es el caso de coordenadas se podrá observar el mapa.
+
+
+Para ejecutarlo, es necesario reconstruir los módulos de node usando el siguiente comando. Así como también debe tener instalado Cordova.
+
+```
+npm install
+
+```
+
+Para correr la App debe ejecutar el siguiente comando, en la carpeta del proyecto
+
+```
+ionic serve
+
+```
---
+++ src/app/pages/mapa/mapa.page.ts
@@ -0,0 +1,109 @@
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+declare var mapboxgl: any;
+import { environment } from '../../../environments/environment';
+
+
+@Component({
+  selector: 'app-mapa',
+  templateUrl: './mapa.page.html',
+  styleUrls: ['./mapa.page.scss'],
+})
+export class MapaPage implements OnInit, AfterViewInit {
+
+	lat: number;
+	lng: number;
+
+  constructor(private activatedRoute: ActivatedRoute) { }
+
+  ngOnInit() {
+
+  	let geo:any = this.activatedRoute.snapshot.paramMap.get('geo');
+
+  	geo = geo.substr(4);
+  	geo = geo.split(',');
+
+  	this.lat = Number(geo[0]);
+  	this.lng = Number(geo[1]);
+
+  	console.log(this.lat, this.lng);
+  }
+
+
+  ngAfterViewInit(){
+  						// para la apiKey de mapbox, debe registrarse y generarla, y luego sustituirlas			
+  	mapboxgl.accessToken = 'tu_apiKey_de_mapbox';
+	const map = new mapboxgl.Map({
+		style: 'mapbox://styles/mapbox/light-v10',
+		center: [this.lng, this.lat],
+		zoom: 15.5,
+		pitch: 45,
+		bearing: -17.6,
+		container: 'map',
+		antialias: true
+	});
+
+	// The 'building' layer in the mapbox-streets vector source contains building-height
+	// data from OpenStreetMap.
+	map.on('load', () => {
+
+		map.resize();
+
+		//Marker
+		new mapboxgl.Marker()
+		.setLngLat([this.lng, this.lat])
+		.addTo(map);
+
+	// Insert the layer beneath any symbol layer.
+	const layers = map.getStyle().layers;
+	 
+	let labelLayerId;
+	for (let i = 0; i < layers.length; i++) {
+		if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
+			labelLayerId = layers[i].id;
+			break;
+		}
+	}
+	 
+	map.addLayer(
+	{
+		'id': '3d-buildings',
+		'source': 'composite',
+		'source-layer': 'building',
+		'filter': ['==', 'extrude', 'true'],
+		'type': 'fill-extrusion',
+		'minzoom': 15,
+		'paint': {
+		'fill-extrusion-color': '#aaa',
+		 
+		// use an 'interpolate' expression to add a smooth transition effect to the
+		// buildings as the user zooms in
+		'fill-extrusion-height': [
+			'interpolate',
+			['linear'],
+			['zoom'],
+			15,
+			0,
+			15.05,
+			['get', 'height']
+		],
+		'fill-extrusion-base': [
+			'interpolate',
+			['linear'],
+			['zoom'],
+			15,
+			0,
+			15.05,
+			['get', 'min_height']
+		],
+		'fill-extrusion-opacity': 0.6
+		}
+	},
+	labelLayerId
+	);
+
+	});
+
+  }
+
+}
